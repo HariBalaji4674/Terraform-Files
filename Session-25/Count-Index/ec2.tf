5fec1be4b802f22c8e482a6ce060bf2e644cf94b
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-20 10:26:36 +0530

    Adding All Files for for the first Commit

--- Session-25/Count-Index/ec2.tf
+++ Session-25/Count-Index/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Count_index" {
     count = 10
     ami = var.ami_id
-    instance_type = var.instance_names == "mongodb" || var.instance_names == "mysql" ? "t3.medium" : "t2.micro"
+    instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" ? "t3.medium" : "t2.micro"
     tags = {
       Name = var.instance_names[count.index]
     }
