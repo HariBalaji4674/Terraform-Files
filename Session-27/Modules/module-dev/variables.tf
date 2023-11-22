6405cf410daa24c7ea9a85dcc5789ad4dec5eb23
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-22 11:02:43 +0530

    Adding All Files for for the first Commit

---
+++ Session-27/Modules/module-dev/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    type = string
+  default = ""
+}
+variable "instance_type" {
+    type  = string
+    default = " "
+}
+variable "inst_name" {
+    type  = string
+    default = ""
+}
